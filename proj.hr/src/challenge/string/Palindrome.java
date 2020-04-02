93f2ba66f80d59408842d6d090c86e084bf75d26
aadhirai <aasivakumar@paypal.com>
2020-04-02 20:57:01 +0530

    Palindrome

---
+++ proj.hr/src/challenge/string/Palindrome.java
@@ -0,0 +1,23 @@
+package challenge.string;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        System.out.println("madam is a palindrom: "+isPalindrome("madame"));
+    }
+
+    public static boolean isPalindrome(String str){
+        int i = 0;
+        int j = str.length()-1;
+        for(; i < str.length()/2; i++){
+            if( str.charAt(i) == str.charAt(j) ){
+                j--;
+                continue;
+            }
+            else{
+                return false;
+            }
+
+        }
+        return true;
+    }
+}
